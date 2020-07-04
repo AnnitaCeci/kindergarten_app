526fbf71f36a5191b7f81295f885e33bf0127c24
AnnitaCeci <anaceci.perezmorales@gmail.com>
2020-07-04 01:29:00 -0500

    add activities api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #
+  resources :activity_logs, only: [:index]
+
+  namespace :api, defaults: { format: :json } do
+    #list of resources
+    resources :activities
+  end
 end
