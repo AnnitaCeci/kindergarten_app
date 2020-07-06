990418ee8fb21e95221a27be5331546e821dfe0e
AnnitaCeci <anaceci.perezmorales@gmail.com>
2020-07-06 02:11:16 -0500

    add filters

--- app/controllers/activity_logs_controller.rb
+++ app/controllers/activity_logs_controller.rb
@@ -2,7 +2,10 @@ class ActivityLogsController < ApplicationController
 
   def index
     @activity_logs = ActivityLog.all.order(start_time: :desc)
-    @babies = Baby.all
-    puts @babies.each { |baby| puts baby.id}
+    if (params[:baby_id].present? || params[:assistant_id].present?)
+      @activity_logs = ActivityLog.where("baby_id = ? OR assistant_id = ? ", params[:baby_id], params[:assistant_id]).order(start_time: :desc)
+    end
   end
+
+
 end
--- app/helpers/activity_logs_helper.rb
+++ app/helpers/activity_logs_helper.rb
@@ -1,10 +1,15 @@
 module ActivityLogsHelper
-  STATUS = ["Terminada", "En Progreso"]
+
+  STATUS = ['Terminada', 'En Progreso']
+
   def status(duration)
-    duration ? "Terminada" : "En Progreso"
+    duration ? 'Terminada' : 'En Progreso'
   end
 
   def duration(duration)
-    duration ? duration.to_s + " min" : "*"
+    duration ? duration.to_s + ' min' : '*'
+  end
+  def format_date(date)
+    date.strftime '%Y-%m-%d %H:%M'
   end
 end
